dc330a6bd995159c4a5ff55a74f55def6d86b655
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2023-11-24 17:25:51 +0700

    update appearance

--- index.php
+++ index.php
@@ -23,34 +23,67 @@ if (isset($_POST['login'])) {
 }
  
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Log in</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="assets/css/adminlte.min.css">
 </head>
-<body>
-    
-        <h1>Login</h1>
-        
-            <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
-                <table>
-                    <tr>
-                        <td><label for="username" >Username</label></td>
-                        <td><input type="text" id="username" name="username"></td>
-                    </tr>
-                    <tr>
-                        <td><label for="password" >Password</label></td>
-                        <td><input type="password" id="password" name="password"></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><input type="submit" value="login" name="login"></td>
-                    </tr>
-                </table>
-            </form>
-        
-    
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="#" class="h1"><b>TOKO</b>RIZ</a>
+    </div>
+    <div class="card-body">
+      <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+        <div class="input-group mb-3">
+          <input type="text" id="username" name="username" class="form-control" placeholder="Username" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" name="login" class="btn btn-primary btn-block">Log In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/js/adminlte.min.js"></script>
 </body>
 </html>
